fee39cb00ee89fa6b04139a3a0678a427016716b
WelcomeLegend-Git <your-email@example.com>
2026-03-14 00:23:13 +0530

    change for release verison

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,11 +22,13 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
@@ -58,6 +60,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    // Upgrades fragment to fix InvalidFragmentVersionForActivityResult Lint error
+    implementation("androidx.fragment:fragment-ktx:1.8.1")
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
